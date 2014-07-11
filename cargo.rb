940e43c31010c7435090d0d3c67c7192f0802b03
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-07-11 13:05:25 +0200

    Formula for cargo

---
+++ cargo.rb
@@ -0,0 +1,20 @@
+# https://github.com/nerdrew/homebrew-tap/blob/3297028bf1d6dd6b283a896475d846b102227109/cargo.rb
+
+require "formula"
+
+class Cargo < Formula
+  homepage "https://github.com/rust-lang/cargo"
+  head "https://github.com/rust-lang/cargo.git"
+
+  depends_on 'rust'
+
+  def install
+    args = ["PREFIX=#{prefix}"]
+    system "make", *args
+    system "make", "install", *args
+  end
+
+  test do
+    system "#{bin}/cargo"
+  end
+end
